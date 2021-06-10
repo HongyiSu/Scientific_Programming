48e8e91595002ed28ff30db4e53c98551f0509c6
hongyi su <hongyi.su1@ucalgary.ca>
2021-06-10 21:49:48 +0200

    add

---
+++ Code/sprintf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+char fmt[80];
+//fName = genFileName( "iteration", 9, 12 );
+for(int i=0;i<=12;i++){
+   int n = sprintf(fmt, "iteration%02d.data",i);
+   printf("%s\n",fmt);
+  }
+
+
+//fName = genFileName( "output", 21, 80 );
+for(int i=0;i<=80;i++){
+   int n = sprintf(fmt, "output%02d.data",i);
+   printf("%s\n",fmt);
+  }
+
+
+//fName = genFileName( "timestep", 123, 10000 );
+for(int i=0;i<=10000;i++){
+   int n = sprintf(fmt, "timestep%05d.data",i);
+   printf("%s\n",fmt);
+  }
+}
